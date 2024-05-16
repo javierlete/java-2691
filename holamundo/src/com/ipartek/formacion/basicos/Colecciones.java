c9e179506aee60a0b6a4aa43cd68a65fca1a363c
Curso <Curso@server.ipartekaula.com>
2024-05-16 15:34:41 +0200

    Colecciones

---
+++ holamundo/src/com/ipartek/formacion/basicos/Colecciones.java
@@ -0,0 +1,57 @@
+package com.ipartek.formacion.basicos;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Colecciones {
+	public static void main(String[] args) {
+		var lista = new ArrayList<String>();
+		
+		lista.add("Uno");
+		lista.add("Dos");
+		lista.add("Tres");
+		lista.add("Tres");
+		
+		lista.remove("Dos");
+		lista.add(0, "Cero");
+		
+		for(var dato: lista) {
+			System.out.println(dato);
+		}
+		
+		System.out.println(lista.get(0));
+		
+		System.out.println(lista);
+		
+		var conjunto = new HashSet<String>();
+		
+		conjunto.add("Uno");
+		conjunto.add("Dos");
+		conjunto.add("Dos");
+		conjunto.add("Tres");
+		
+		System.out.println(conjunto);
+		
+		var mapa = new HashMap<String, Integer>();
+		
+		mapa.put("Uno", 1);
+		mapa.put("Dos", 2);
+		
+		System.out.println(mapa.get("Dos"));
+		
+		System.out.println(mapa);
+		
+		for(var par: mapa.entrySet()) {
+			System.out.println(par.getKey() + ": " + par.getValue());
+		}
+		
+		for(var clave: mapa.keySet()) {
+			System.out.println(clave + ": " + mapa.get(clave));
+		}
+		
+		for(var valor: mapa.values()) {
+			System.out.println(valor);
+		}
+	}
+}
