a1d2bf278e36b9a8747c5f82474ee16ff436941b
Curso <Curso@server.ipartekaula.com>
2024-06-03 15:44:07 +0200

    Completado de las clases

--- veterinario/src/com/ipartek/formacion/veterinario/entidades/Servicio.java
+++ veterinario/src/com/ipartek/formacion/veterinario/entidades/Servicio.java
@@ -1,6 +1,7 @@
 package com.ipartek.formacion.veterinario.entidades;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Servicio {
 	private Long id;
@@ -8,4 +9,82 @@ public class Servicio {
 	private String descripcion;
 	private BigDecimal precio;
 	private Integer minutos;
+
+	public Servicio(Long id, String nombre, String descripcion, BigDecimal precio, Integer minutos) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.precio = precio;
+		this.minutos = minutos;
+	}
+
+	public Servicio() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public BigDecimal getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(BigDecimal precio) {
+		this.precio = precio;
+	}
+
+	public Integer getMinutos() {
+		return minutos;
+	}
+
+	public void setMinutos(Integer minutos) {
+		this.minutos = minutos;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(descripcion, id, minutos, nombre, precio);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Servicio other = (Servicio) obj;
+		return Objects.equals(descripcion, other.descripcion) && Objects.equals(id, other.id)
+				&& Objects.equals(minutos, other.minutos) && Objects.equals(nombre, other.nombre)
+				&& Objects.equals(precio, other.precio);
+	}
+
+	@Override
+	public String toString() {
+		return "Servicio [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", precio=" + precio
+				+ ", minutos=" + minutos + "]";
+	}
+
 }
--- veterinario/src/com/ipartek/formacion/veterinario/entidades/Veterinario.java
+++ veterinario/src/com/ipartek/formacion/veterinario/entidades/Veterinario.java
@@ -1,4 +1,6 @@
 package com.ipartek.formacion.veterinario.entidades;
 
 public class Veterinario extends Persona {
+
+	private static final long serialVersionUID = 522319412543737570L;
 }
