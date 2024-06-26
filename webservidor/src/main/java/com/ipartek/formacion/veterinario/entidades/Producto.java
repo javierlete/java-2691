b061a6c179d82e0bf6b84c09de874114f6e578dc
Curso <Curso@server.ipartekaula.com>
2024-06-26 15:52:47 +0200

    "Lombokización" de Producto

--- webservidor/src/main/java/com/ipartek/formacion/veterinario/entidades/Producto.java
+++ webservidor/src/main/java/com/ipartek/formacion/veterinario/entidades/Producto.java
@@ -1,90 +1,21 @@
 package com.ipartek.formacion.veterinario.entidades;
 
 import java.math.BigDecimal;
-import java.util.Objects;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// LOMBOK
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Producto {
 	private Long id;
 	private String nombre;
 	private String descripcion;
 	private BigDecimal precio;
 	private Integer stock;
-
-	public Producto(Long id, String nombre, String descripcion, BigDecimal precio, Integer stock) {
-		super();
-		this.id = id;
-		this.nombre = nombre;
-		this.descripcion = descripcion;
-		this.precio = precio;
-		this.stock = stock;
-	}
-
-	public Producto() {
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getDescripcion() {
-		return descripcion;
-	}
-
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}
-
-	public BigDecimal getPrecio() {
-		return precio;
-	}
-
-	public void setPrecio(BigDecimal precio) {
-		this.precio = precio;
-	}
-
-	public Integer getStock() {
-		return stock;
-	}
-
-	public void setStock(Integer stock) {
-		this.stock = stock;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(descripcion, id, nombre, precio, stock);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Producto other = (Producto) obj;
-		return Objects.equals(descripcion, other.descripcion) && Objects.equals(id, other.id)
-				&& Objects.equals(nombre, other.nombre) && Objects.equals(precio, other.precio)
-				&& Objects.equals(stock, other.stock);
-	}
-
-	@Override
-	public String toString() {
-		return "Producto [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", precio=" + precio
-				+ ", stock=" + stock + "]";
-	}
-
 }
