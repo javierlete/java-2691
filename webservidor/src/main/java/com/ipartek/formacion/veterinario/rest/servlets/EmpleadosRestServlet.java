cefebd67b1dcaadc786732eb8229b51e46f2d1d4
Curso <Curso@server.ipartekaula.com>
2024-07-01 17:46:33 +0200

    Refactorización de lo anterior

--- webservidor/src/main/java/com/ipartek/formacion/veterinario/rest/servlets/EmpleadosRestServlet.java
+++ webservidor/src/main/java/com/ipartek/formacion/veterinario/rest/servlets/EmpleadosRestServlet.java
@@ -24,44 +24,21 @@ public class EmpleadosRestServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String pathInfo = request.getPathInfo();
-
 		PrintWriter out = response.getWriter();
 
 		DaoEmpleado dao = DaoEmpleadoJpa.getInstancia();
 
-		if (pathInfo == null) {
+		Long id = getIdPath(request);
+		
+		if (id == null) {
 
 			var empleados = dao.obtenerTodos();
 
-			out.write("[");
-
-			boolean esPrimerRegistro = true;
-
-			for (Empleado e : empleados) {
-				if (!esPrimerRegistro) {
-					out.println(", ");
-				}
-
-				out.printf(
-						"""
-									{ "id": %s, "nombre": "%s", "apellidos": "%s", "nif": "%s", "nss": "%s", "sueldoMensual": "%s" }
-								""",
-						e.getId(), e.getNombre(), e.getApellidos(), e.getNif(), e.getNss(), e.getSueldoMensual());
-
-				esPrimerRegistro = false;
-			}
-
-			out.write("]");
+			enviarEmpleados(out, empleados);
 		} else {
-			Long id = Long.parseLong(pathInfo.substring(1));
-
 			Empleado e = dao.obtenerPorId(id);
 
-			out.printf("""
-						{ "id": %s, "nombre": "%s", "apellidos": "%s", "nif": "%s", "nss": "%s", "sueldoMensual": "%s" }
-					""", e.getId(), e.getNombre(), e.getApellidos(), e.getNif(), e.getNss(), e.getSueldoMensual());
-
+			enviarEmpleado(out, e);
 		}
 	}
 
@@ -69,12 +46,82 @@ public class EmpleadosRestServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
+		
+		Empleado empleado = extraerEmpleado(request);
+
+		DaoEmpleado dao = DaoEmpleadoJpa.getInstancia();
+
+		var e = dao.insertar(empleado);
+
+		enviarEmpleado(out, e);
+	}
+
+	@Override
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		
+		Empleado empleado = extraerEmpleado(request);
+
+		DaoEmpleado dao = DaoEmpleadoJpa.getInstancia();
+
+		var e = dao.modificar(empleado);
+
+		enviarEmpleado(out, e);
+	}
+
+
+	@Override
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		DaoEmpleado dao = DaoEmpleadoJpa.getInstancia();
+
+		Long id = getIdPath(request);
+		
+		if (id != null) {
+			dao.borrar(id);
+		}
+	}
+	
+	private Long getIdPath(HttpServletRequest request) {
+		String pathInfo = request.getPathInfo();
+		
+		return pathInfo == null ? null : Long.parseLong(pathInfo.substring(1));
+	}
+
+	private void enviarEmpleados(PrintWriter out, Iterable<Empleado> empleados) {
+		out.write("[");
+	
+		boolean esPrimerRegistro = true;
+	
+		for (Empleado e : empleados) {
+			if (!esPrimerRegistro) {
+				out.println(", ");
+			}
+	
+			enviarEmpleado(out, e);
+	
+			esPrimerRegistro = false;
+		}
+	
+		out.write("]");
+	}
+
+	private void enviarEmpleado(PrintWriter out, Empleado e) {
+		out.printf(
+				"""
+							{ "id": %s, "nombre": "%s", "apellidos": "%s", "nif": "%s", "nss": "%s", "sueldoMensual": "%s" }
+						""",
+				e.getId(), e.getNombre(), e.getApellidos(), e.getNif(), e.getNss(), e.getSueldoMensual());
+	}
+
+	private Empleado extraerEmpleado(HttpServletRequest request) throws IOException {
 		BufferedReader br = request.getReader();
-
+	
 		StringBuffer sb = new StringBuffer();
-
+	
 		String linea;
-
+	
 		while ((linea = br.readLine()) != null) {
 			sb.append(linea);
 		}
@@ -93,68 +140,6 @@ public class EmpleadosRestServlet extends HttpServlet {
 		}
 		
 		Empleado empleado = new Empleado(null, dic.get("nombre"), dic.get("apellidos"), dic.get("nif"), dic.get("telefono"), dic.get("nss"), new BigDecimal(dic.get("sueldoMensual")));
-
-		DaoEmpleado dao = DaoEmpleadoJpa.getInstancia();
-
-		var e = dao.insertar(empleado);
-
-		out.printf("""
-					{ "id": %s, "nombre": "%s", "apellidos": "%s", "nif": "%s", "nss": "%s", "sueldoMensual": "%s" }
-				""", e.getId(), e.getNombre(), e.getApellidos(), e.getNif(), e.getNss(), e.getSueldoMensual());
-
-	}
-	
-	@Override
-	protected void doPut(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		BufferedReader br = request.getReader();
-
-		StringBuffer sb = new StringBuffer();
-
-		String linea;
-
-		while ((linea = br.readLine()) != null) {
-			sb.append(linea);
-		}
-		
-		String json = sb.toString();
-		
-		json = json.replace("{", "").replace("}", "").replace("\"", "");
-		
-		TreeMap<String, String> dic = new TreeMap<>();
-		
-		String[] pares = json.split(",");
-		
-		for(String textoPar: pares) {
-			String[] par = textoPar.split(":");
-			dic.put(par[0].trim(), par[1].trim());
-		}
-		
-		Empleado empleado = new Empleado(Long.parseLong(dic.get("id")), dic.get("nombre"), dic.get("apellidos"), dic.get("nif"), dic.get("telefono"), dic.get("nss"), new BigDecimal(dic.get("sueldoMensual")));
-
-		DaoEmpleado dao = DaoEmpleadoJpa.getInstancia();
-
-		var e = dao.modificar(empleado);
-
-		out.printf("""
-					{ "id": %s, "nombre": "%s", "apellidos": "%s", "nif": "%s", "nss": "%s", "sueldoMensual": "%s" }
-				""", e.getId(), e.getNombre(), e.getApellidos(), e.getNif(), e.getNss(), e.getSueldoMensual());
-
-	}
-
-
-	@Override
-	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String pathInfo = request.getPathInfo();
-
-		DaoEmpleado dao = DaoEmpleadoJpa.getInstancia();
-
-		if (pathInfo != null) {
-			Long id = Long.parseLong(pathInfo.substring(1));
-
-			dao.borrar(id);
-		}
+		return empleado;
 	}
 }
