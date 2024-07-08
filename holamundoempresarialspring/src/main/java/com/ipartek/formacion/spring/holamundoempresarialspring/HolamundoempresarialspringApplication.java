7082d238fa7002309e963d16cc5fba9ab4d5b24b
Curso <Curso@server.ipartekaula.com>
2024-07-08 16:22:51 +0200

    Spring Starter Project

---
+++ holamundoempresarialspring/src/main/java/com/ipartek/formacion/spring/holamundoempresarialspring/HolamundoempresarialspringApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.holamundoempresarialspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolamundoempresarialspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolamundoempresarialspringApplication.class, args);
+	}
+
+}
