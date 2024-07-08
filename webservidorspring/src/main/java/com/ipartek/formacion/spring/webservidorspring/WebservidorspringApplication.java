c98743b220d4e9f82e9e29d4eb2e414a8b294129
Curso <Curso@server.ipartekaula.com>
2024-07-08 16:39:03 +0200

    webservidorspring: Lombok, MySQL Driver, Validation, Spring Data JPA

---
+++ webservidorspring/src/main/java/com/ipartek/formacion/spring/webservidorspring/WebservidorspringApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.webservidorspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservidorspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservidorspringApplication.class, args);
+	}
+
+}
---
+++ webservidorspring/src/test/java/com/ipartek/formacion/spring/webservidorspring/WebservidorspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.webservidorspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservidorspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
