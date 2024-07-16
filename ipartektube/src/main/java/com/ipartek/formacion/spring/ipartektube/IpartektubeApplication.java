f2881390c36303e7cdf3eee7bb83ff21132c5337
Curso <Curso@server.ipartekaula.com>
2024-07-16 16:26:23 +0200

    Proyecto Ipartektube con todos los starters + thymeleaf-security

---
+++ ipartektube/src/main/java/com/ipartek/formacion/spring/ipartektube/IpartektubeApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.ipartektube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpartektubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpartektubeApplication.class, args);
+	}
+
+}
---
+++ ipartektube/src/test/java/com/ipartek/formacion/spring/ipartektube/IpartektubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.ipartektube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpartektubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
